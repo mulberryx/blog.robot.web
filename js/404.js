df9093003c3caed795ab1dc5f782f36182f9c232
Mulberry <wowcxy2008@126.com>
2018-01-10 09:31:34 +0800

    auto#ignore

--- js/404.js
+++ js/404.js
@@ -6,128 +6,80 @@ function(e) {
     input.focus();
 });
 
-input.on('keyup',
-function(e) {
-    $('.new-output').text(input.val());
-    // console.log(inputReady);
+input.on('keyup', function(e) {
+  $('.new-output').text(input.val());
 });
 
-$('.four-oh-four-form').on('submit', function(e) {
-    e.preventDefault();
-    var val = $(this).children($('.404-input')).val().toLowerCase();
-    var href;
+$('.four-oh-four-form').on('submit', function (e) {
+  e.preventDefault();
+  var val = $(this).children($('.404-input')).val().toLowerCase();
+  var href;
 
-    if (val === 'kittens') {
-        showKittens();
-    } else {
-        resetForm();
-    }
+  if (val === 'hack') {
+      hack();
+  } else {
+      resetForm();
+  }
 });
 
-function resetForm(withKittens) {
-    var message = "Sorry that command is not recognized."
-    var input = $('.404-input');
+function hack () {
 
-    if (withKittens) {
-        $('.kittens').removeClass('kittens');
-        message = "Huzzzzzah Kittehs!"
-    }
-
-    $('.new-output').removeClass('new-output');
-    input.val('');
-    $('.terminal').append('<p class="prompt">' + message + '</p><p class="prompt output new-output"></p>');
-
-    $('.new-output').velocity('scroll'), 
-    {
-        duration: 100
-    }
 }
 
-var showKittens = function() {
-    $('.terminal').append("<div class='kittens'>" 
-                            + "<p class='prompt'>                            ,----,         ,----,                                          ,---,</p>" 
-                            + "<p class='prompt'>       ,--.                /   .`|       ,/   .`|                     ,--.              ,`--.' |</p>" 
-                            + "<p class='prompt'>   ,--/  /|    ,---,     ,`   .'  :     ,`   .'  :     ,---,.        ,--.'|   .--.--.    |   :  :</p>" 
-                            + "<p class='prompt'>,---,': / ' ,`--.' |   ;    ;     /   ;    ;     /   ,'  .' |    ,--,:  : |  /  /    '.  '   '  ;</p>" 
-                            + "<p class='prompt'>:   : '/ /  |   :  : .'___,/    ,'  .'___,/    ,'  ,---.'   | ,`--.'`|  ' : |  :  /`. /  |   |  |</p>" 
-                            + "<p class='prompt'>|   '   ,   :   |  ' |    :     |   |    :     |   |   |   .' |   :  :  | | ;  |  |--`   '   :  ;</p>" 
-                            + "<p class='prompt'>'   |  /    |   :  | ;    |.';  ;   ;    |.';  ;   :   :  |-, :   |   \\ | : |  :  ;_     |   |  '</p>" 
-                            + "<p class='prompt'>|   ;  ;    '   '  ; `----'  |  |   `----'  |  |   :   |  ;/| |   : '  '; |  \\  \\    `.  '   :  |</p>" 
-                            + "<p class='prompt'>:   '   \\   |   |  |     '   :  ;       '   :  ;   |   :   .' '   ' ;.    ;   `----.   \\ ;   |  ;</p>" 
-                            + "<p class='prompt'>'   : |.  \\ |   |  '     '   :  |       '   :  |   '   :  ;/| '   : |  ; .'  /  /`--'  /  `--..`;  </p>" 
-                            + "<p class='prompt'>|   | '_\\.' '   :  |     ;   |.'        ;   |.'    |   |    \\ |   | '`--'   '--'.     /  .--,_   </p>" 
-                            + "<p class='prompt'>'   : |     ;   |.'      '---'          '---'      |   :   .' '   : |         `--'---'   |    |`.  </p>" 
-                            + "<p class='prompt'>;   |,'     '---'                                  |   | ,'   ;   |.'                    `-- -`, ; </p>" 
-                            + "<p class='prompt'>'---'                                              `----'     '---'                        '---`'</p>" 
-                            + "<p class='prompt'>                                                              </p></div>");
+function resetForm(withHack) {
+  var message = "Sorry that command is not recognized."
+  var input = $('.404-input');
 
-    var lines = $('.kittens p');
-    $.each(lines, function(index, line) {
-        setTimeout(function() {
-            $(line).css({
-                "opacity": 1
-            });
+  if (withHack) {
+    $('.hack').removeClass('hack');
+    message = "Huzzzzzah Hack!";
+  }
 
-            textEffect($(line))
-        },
-        index * 100);
-    });
+  $('.new-output').removeClass('new-output');
+  input.val('');
+  $('.terminal').append('<p class="prompt">' + message + '</p><p class="prompt output new-output"></p>');
 
-    $('.new-output').velocity('scroll'),
-    {
-        duration: 100
-    }
-
-    setTimeout(function() {
-        var gif;
-
-        $.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=kittens',
-        function(result) {
-            gif = result.data.image_url;
-            $('.terminal').append('<img class="kitten-gif" src="' + gif + '"">');
-            resetForm(true);
-        });
-    },
-    (lines.length * 100) + 1000);
+  $('.new-output').velocity('scroll'), {
+      duration: 100
+  }
 }
 
 var textEffect = function (line) {
-    var alpha = [';', '.', ',', ':', ';', '~', '`'];
-    var animationSpeed = 10;
-    var index = 0;
-    var string = line.text();
-    var splitString = string.split("");
-    var copyString = splitString.slice(0);
+  var alpha = [';', '.', ',', ':', ';', '~', '`'];
+  var animationSpeed = 10;
+  var index = 0;
+  var string = line.text();
+  var splitString = string.split("");
+  var copyString = splitString.slice(0);
 
-    var emptyString = copyString.map(function(el) {
-        return [alpha[Math.floor(Math.random() * (alpha.length))], index++];
-    })
+  var emptyString = copyString.map(function(el) {
+      return [alpha[Math.floor(Math.random() * (alpha.length))], index++];
+  })
 
-    emptyString = shuffle(emptyString);
+  emptyString = shuffle(emptyString);
 
-    $.each(copyString,
-    function(i, el) {
-        var newChar = emptyString[i];
-        toUnderscore(copyString, line, newChar);
+  $.each(copyString, function(i, el) {
+    var newChar = emptyString[i];
+    toUnderscore(copyString, line, newChar);
 
-        setTimeout(function() {
-            fromUnderscore(copyString, splitString, newChar, line);
-        },
-        i * animationSpeed);
-    })
+    setTimeout(function() {
+        fromUnderscore(copyString, splitString, newChar, line);
+    },
+    i * animationSpeed);
+  });
 }
 
 var toUnderscore = function (copyString, line, newChar) {
-    copyString[newChar[1]] = newChar[0];
-    line.text(copyString.join(''));
+  copyString[newChar[1]] = newChar[0];
+  line.text(copyString.join(''));
 }
 
 var fromUnderscore = function (copyString, splitString, newChar, line) {
-    copyString[newChar[1]] = splitString[newChar[1]];
-    line.text(copyString.join(""));
+  copyString[newChar[1]] = splitString[newChar[1]];
+  line.text(copyString.join(""));
 }
 
 var shuffle = function (o) {
-    for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-    return o;
+  for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+  return o;
 };
