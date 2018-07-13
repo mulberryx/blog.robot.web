21d6af8db78b7227817d5df09ceab9783cdc63e9
Steudnera C. Koch <wowcxy2008@126.com>
2018-07-13 10:23:31 +0800

    Update webpack.base.conf.js

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -16,7 +16,7 @@ const createLintingRule = () => ({
   options: {
     formatter: require('eslint-friendly-formatter'),
     emitWarning: !config.dev.showEslintErrorsInOverlay
-  }
+  },
 })
 
 module.exports = {
@@ -24,19 +24,20 @@ module.exports = {
   entry: {
     index: './js/index.js',
     talk: './js/talk.js',
+    admin: './js/admin/admin.js',
   },
   output: {
     path: config.build.assetsRoot,
     filename: 'js/[name].js',
     publicPath: process.env.NODE_ENV === 'production'
       ? config.build.assetsPublicPath
-      : config.dev.assetsPublicPath
+      : config.dev.assetsPublicPath,
   },
   resolve: {
     extensions: ['.js', '.json'],
     alias: {
       '@': resolve('js'),
-    }
+    },
   },
   module: {
     rules: [
@@ -44,31 +45,31 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
+        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')],
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: utils.assetsPath('img/[name].[ext]')
-        }
+          name: utils.assetsPath('img/[name].[ext]'),
+        },
       },
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: utils.assetsPath('media/[name].[ext]')
-        }
+          name: utils.assetsPath('media/[name].[ext]'),
+        },
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: utils.assetsPath('fonts/[name].[ext]')
-        }
+          name: utils.assetsPath('fonts/[name].[ext]'),
+        },
       }
     ]
   },
@@ -82,6 +83,6 @@ module.exports = {
     fs: 'empty',
     net: 'empty',
     tls: 'empty',
-    child_process: 'empty'
+    child_process: 'empty',
   }
 }
